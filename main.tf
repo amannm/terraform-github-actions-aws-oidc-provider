73d0a0304525b3ec307b1c3eacc3d14af7bcea93
Amann Malik <amannmalik@gmail.com>
2022-01-15 19:52:37 -0600

    revert fix

--- main.tf
+++ main.tf
@@ -9,9 +9,8 @@ data "tls_certificate" "provider-certificate" {
   url = "https://token.actions.githubusercontent.com"
 }
 resource "aws_iam_openid_connect_provider" "provider" {
-  url = data.tls_certificate.provider-certificate.url
-  #  https://github.blog/changelog/2022-01-13-github-actions-update-on-oidc-based-deployments-to-aws/
-  thumbprint_list = concat(["6938fd4d98bab03faadb97b34396831e3780aea1"], slice([for c in data.tls_certificate.provider-certificate.certificates : c.sha1_fingerprint], 0, min(length(data.tls_certificate.provider-certificate.certificates), 5)))
+  url             = data.tls_certificate.provider-certificate.url
+  thumbprint_list = slice([for c in data.tls_certificate.provider-certificate.certificates : c.sha1_fingerprint], 0, min(length(data.tls_certificate.provider-certificate.certificates), 5))
   client_id_list  = ["sts.amazonaws.com"]
 }
 module "provider-role" {
